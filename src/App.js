d4100462091252f821fc209edbd1e4cb5b34d083
vpsingh12 <59245277+vpsingh12@users.noreply.github.com>
2020-03-25 13:19:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import axios from "axios";
+  
 
-function App() {
+
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      posts:[]
+    }
+  }
+
+  componentWillMount () {
+    localStorage.getItem("posts") && this.setState({
+      posts:JSON.parse(localStorage.getItem("posts"))
+    })
+  }
+  componentDidMount () {
+    if (!localStorage.getItem("posts")) {
+      axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+       // const posts = res.data;
+        this.setState({ posts:res.data });
+        
+      })
+    } else {
+      console.log("using data from local storage");
+    }
+    
+  }
+
+  componentWillUpdate (nextProp , nextState) {
+    localStorage.setItem("posts",JSON.stringify(nextState.posts));
+  }
+  render() {
+    const columns = [{  
+      Header: 'Id',  
+      accessor: 'id',
+      width: 100,
+      minWidth:100 ,
+      maxWidth:100 ,
+      filterable:true
+      },
+      {  
+      Header: 'Title',  
+      accessor: 'title',
+      sortable : false ,
+      filterable: true
+      },
+      {  
+      Header: 'Content',  
+      accessor: 'body',
+      sortable: false ,
+      filterable: true
+        }] 
+        
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "react-classes">
+     <ReactTable 
+                columns = {columns}
+                data = {this.state.posts}
+                defaultPageSize = {10}  
+                pageSizeOptions = {[10,20]}
+                
+     />
     </div>
+                 
   );
 }
-
+}
 export default App;
