00cd9482b8a2f4a637c75200298ab1eb8650301b
vpsingh12 <59245277+vpsingh12@users.noreply.github.com>
2020-03-27 16:25:09 +0530

    Updates

---
+++ precache-manifest.c92dc4d26cc6d7569b6f996702968786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fb32e4c138db6c36ffa1363cd55730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2320f98955f9a64123b",
+    "url": "/static/css/main.76bd0105.chunk.css"
+  },
+  {
+    "revision": "a284fe616e47d3ef31cf",
+    "url": "/static/js/2.e6206bc9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e6206bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2320f98955f9a64123b",
+    "url": "/static/js/main.c6f43409.chunk.js"
+  },
+  {
+    "revision": "656aa50fbad87425b122",
+    "url": "/static/js/runtime-main.b1cc144f.js"
+  }
+]);
